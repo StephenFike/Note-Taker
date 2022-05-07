44dc3c0a3941b0b826fa3f0f19153c4e32c16d89
Stephen Fike <sfike98@gmail.com>
2022-05-07 12:05:44 -0500

    adding ability to update notes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,11 +2,16 @@ const router = require('express').Router();
 const path = require('path');
 const fs = require('fs');
 const notes = require('../Develop/db/db.json');
+const { updateNote, findNote } = require('../lib/notes')
 
 // Get all notes
 router.get('/notes', (req, res) => {
-    let results = notes;
-    res.json(results);
+    res.json(notes);
+})
+
+// update note
+router.put('/notes/:id', (req,res) => {
+    findNote(req.params.id, req.params.body)
 })
 
 module.exports = router;
