0f27a6243203baca634e4b714f0dc0147cab59fd
Stephen Fike <sfike98@gmail.com>
2022-05-07 21:03:40 -0500

    can now update and delete notes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,16 +2,41 @@ const router = require('express').Router();
 const path = require('path');
 const fs = require('fs');
 const notes = require('../Develop/db/db.json');
-const { updateNote, findNote } = require('../lib/notes')
+const uuid = require('uuid');
 
 // Get all notes
 router.get('/notes', (req, res) => {
     res.json(notes);
-})
+});
 
-// update note
-router.put('/notes/:id', (req,res) => {
-    findNote(req.params.id, req.params.body)
-})
+// post note
+router.post('/notes', (req,res) => {
+
+    const newNote = req.body;
+    newNote.id = uuid.v4();
+
+    notes.push(newNote);
+
+    fs.writeFileSync('./Develop/db/db.json', JSON.stringify(notes), function(err) {
+        if(err) {
+            return console.log(err)
+        }
+    })
+    res.json(notes);
+});
+
+// delete a note
+router.delete('/notes/:id', (req, res) => {
+    
+    const deletedNote = notes.filter(delNote => delNote.id !== req.params.id)
+
+    fs.writeFileSync('./Develop/db/db.json', JSON.stringify(deletedNote), function(err) {
+        if(err) {
+            return console.log(err)
+        }
+    })
+
+    res.json(notes);
+});
 
 module.exports = router;
--- server.js
+++ server.js
@@ -3,8 +3,8 @@ const apiRoutes = require('./routes/apiRoutes');
 const htmlRoutes = require('./routes/htmlRoutes');
 
 const PORT = process.env.PORT || 3001;
-
 const app = express();
+
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 app.use(express.static('./Develop/public'))
